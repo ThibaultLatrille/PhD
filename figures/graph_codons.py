354df45e4525d0cd4df408074ca88a4a060707c3
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2020-07-08 00:13:24 +0200

    Moved all declaration into thesis.cls, fixed warnings at compilation.

--- figures/graph_codons.py
+++ figures/graph_codons.py
@@ -1,6 +1,4 @@
 import graph_tool.all as gt
-from graph_tool.all import Graph, graph_draw
-from graph_tool.inference.nested_blockmodel import NestedBlockState
 from itertools import product
 import numpy as np
 import matplotlib.pyplot as plt
@@ -37,11 +35,11 @@ for k, v in sorted(codontable.items(), key=lambda kv: kv[0]):
         aa_order.append(v)
 
 
-def codon_circos(cmap='tab20', filetype="pdf"):
+def codon_circos(cmap='tab20', filetype="pdf", reverse=False):
     cm = plt.cm.get_cmap(cmap)
     cmappable = ScalarMappable(norm=Normalize(vmin=0, vmax=20), cmap=cm)
 
-    g_codons = Graph(directed=False)
+    g_codons = gt.Graph(directed=False)
     g_codons.vp.codon = g_codons.new_vertex_property("string")
     g_codons.vp.aa = g_codons.new_vertex_property("string")
     g_codons.vp.aa_index = g_codons.new_vertex_property("int")
@@ -70,6 +68,7 @@ def codon_circos(cmap='tab20', filetype="pdf"):
                 g_codons.ep.syn[e_c] = False
                 x = cmappable.to_rgba(g_codons.vp.aa_index[ref])[:3]
                 y = cmappable.to_rgba(g_codons.vp.aa_index[alt])[:3]
+                if reverse: x, y = y, x
                 g_codons.ep.grad[e_c] = [0.0, *x, 0.75, 1.0, *y, 0.75]
 
     for ref in g_codons.vertices():
@@ -84,26 +83,29 @@ def codon_circos(cmap='tab20', filetype="pdf"):
                 g_codons.ep.grad[e_c] = [0.0, *syn_color, 1.0, *syn_color]
 
     assert g_codons.num_vertices() == 61
+    dist = gt.shortest_distance(g_codons)
+    r = max([max(dist[g_codons.vertex(i)].a) for i in g_codons.vertices()])
+    print('Codons graph radius : {0}'.format(r))
     print('Codons : {0} transitions out of {1} possibles.'.format(g_codons.num_edges(), 61 * 60 / 2))
     syn_array = g_codons.ep.syn.get_array()
     print('Codons : {0} are synonymous and {1} are non-synonymous.'.format(sum(syn_array),
                                                                            len(syn_array) - sum(syn_array)))
-    state = NestedBlockState(g_codons, bs=[g_codons.vp.aa_index, g_codons.vp.codon_index], sampling=False)
+    state = gt.NestedBlockState(g_codons, bs=[g_codons.vp.aa_index, g_codons.vp.codon_index], sampling=False)
     t = gt.get_hierarchy_tree(state)[0]
     tpos = gt.radial_tree_layout(t, t.vertex(t.num_vertices() - 1), weighted=True)
     cts = gt.get_hierarchy_control_points(g_codons, t, tpos)
     pos = g_codons.own_property(tpos)
-    graph_draw(g_codons, pos=pos, edge_control_points=cts, edge_gradient=g_codons.ep.grad, edge_pen_width=2.5,
-               vertex_text=g_codons.vp.codon, vertex_anchor=0, vertex_font_size=9, vertex_pen_width=1.6,
-               vertex_color=(0.65, 0.65, 0.65, 1), vertex_fill_color=g_codons.vp.aa_color, vertex_size=25.0,
-               output="gt-codon-{0}.{1}".format(cmap, filetype))
+    gt.graph_draw(g_codons, pos=pos, edge_control_points=cts, edge_gradient=g_codons.ep.grad, edge_pen_width=2.5,
+                  vertex_text=g_codons.vp.codon, vertex_anchor=0, vertex_font_size=9, vertex_pen_width=1.6,
+                  vertex_color=(0.65, 0.65, 0.65, 1), vertex_fill_color=g_codons.vp.aa_color, vertex_size=25.0,
+                  output="gt-codon-{0}.{1}".format(cmap, filetype))
 
 
-def amino_acid_circos(cmap='tab20', filetype="pdf"):
+def amino_acid_circos(cmap='tab20', filetype="pdf", reverse=False):
     cm = plt.cm.get_cmap(cmap)
     cmappable = ScalarMappable(norm=Normalize(vmin=0, vmax=20), cmap=cm)
 
-    g_aa = Graph(directed=False)
+    g_aa = gt.Graph(directed=False)
     g_aa.vp.aa = g_aa.new_vertex_property("string")
     g_aa.vp.aa_color = g_aa.new_vertex_property("vector<float>")
     g_aa.vp.count = g_aa.new_vertex_property("float")
@@ -128,23 +130,38 @@ def amino_acid_circos(cmap='tab20', filetype="pdf"):
                 e_aa = g_aa.add_edge(ref, alt)
                 x = cmappable.to_rgba(ref_index)[:3]
                 y = cmappable.to_rgba(alt_index)[:3]
+                if reverse: x, y = y, x
                 g_aa.ep.grad[e_aa] = [0.0, *x, 0.75, 1.0, *y, 0.75]
                 g_aa.ep.count[e_aa] = len(nei) * 2.
 
     assert g_aa.num_vertices() == 20
+    dist = gt.shortest_distance(g_aa)
+    r = max([max(dist[g_aa.vertex(i)].a) for i in g_aa.vertices()])
+    print('Amino-acids graph radius : {0}'.format(r))
+    dict_distance = {1: [], 2: [], 3: []}
+    for source in g_aa.vertices():
+        for target in g_aa.vertices():
+            if source <= target: continue
+            dict_distance[int(gt.shortest_distance(g_aa, source, target))].append(
+                "{0}-{1}".format(g_aa.vp.aa[source], g_aa.vp.aa[target]))
+
+    for k, v in dict_distance.items():
+        print("d={0}: {1} pairs".format(k, len(v)))
+        print(", ".join(v))
+
     print('Amino-acids : {0} transitions out of {1} possibles '.format(g_aa.num_edges(), 20 * 19 / 2))
     state = gt.minimize_nested_blockmodel_dl(g_aa, deg_corr=True)
     t = gt.get_hierarchy_tree(state)[0]
     tpos = gt.radial_tree_layout(t, t.vertex(t.num_vertices() - 1), weighted=True)
     cts = gt.get_hierarchy_control_points(g_aa, t, tpos)
     pos = g_aa.own_property(tpos)
-    graph_draw(g_aa, pos=pos, edge_control_points=cts, vertex_anchor=0, vertex_text=g_aa.vp.aa,
-               vertex_fill_color=g_aa.vp.aa_color, vertex_size=g_aa.vp.count, vertex_font_size=16,
-               vertex_pen_width=3.2, vertex_color=(0.65, 0.65, 0.65, 1),
-               edge_gradient=g_aa.ep.grad, edge_pen_width=g_aa.ep.count,
-               output="gt-aa-{0}.{1}".format(cmap, filetype))
+    gt.graph_draw(g_aa, pos=pos, edge_control_points=cts, vertex_anchor=0, vertex_text=g_aa.vp.aa,
+                  vertex_fill_color=g_aa.vp.aa_color, vertex_size=g_aa.vp.count, vertex_font_size=16,
+                  vertex_pen_width=3.2, vertex_color=(0.65, 0.65, 0.65, 1),
+                  edge_gradient=g_aa.ep.grad, edge_pen_width=g_aa.ep.count,
+                  output="gt-aa-{0}.{1}".format(cmap, filetype))
 
 
-for cmap in ['tab20', 'tab20b', 'tab20c']:
-    amino_acid_circos(cmap, "pdf")
-    codon_circos(cmap, "pdf")
+for cmap in ['tab20', 'tab20b']:
+    amino_acid_circos(cmap=cmap, filetype="pdf", reverse=False)
+    codon_circos(cmap=cmap, filetype="pdf", reverse=False)
