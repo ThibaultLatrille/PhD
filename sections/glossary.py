0ac00de68e58ce7b43964cda60076f8550ae4f09
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2020-07-20 17:31:29 +0200

    Glossary entries in the main text.

--- sections/glossary.py
+++ sections/glossary.py
@@ -3,14 +3,16 @@ import argparse
 import inflect
 
 p = inflect.engine()
+input_list = ["intro.tex", "intro_inference.tex", "intro_selection.tex", "discussion.tex", "conclusion.tex",
+              "../MutationSelectionDrift/MutSelDrift-main.tex", "../MutationSelectionDrift/MutSelDrift-supp-mat.tex",
+              "../MutationSelectionDrift/MutSelDrift-main.tex", "../MutationSelectionDrift/MutSelDrift-supp-mat.tex",
+              "../GenotypePhenotypeFitness/GenoPhenoFit-main.tex",
+              "../GenotypePhenotypeFitness/GenoPhenoFit-supp-mat.tex", "../NucleotideBias/Nucleotide-Bias-main.tex"]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-g', '--glossary', required=False, default="glossary.tex", type=str, dest="glossary")
-    parser.add_argument('-i', '--input', required=False,
-                        default=["intro.tex", "intro_inference.tex", "intro_selection.tex", "mut-bias.tex",
-                                 "discussion.tex", "conclusion.tex"], type=str, nargs='+',
-                        dest="input")
+    parser.add_argument('-i', '--input', required=False, default=input_list, type=str, nargs='+', dest="input")
     args = parser.parse_args()
 
     glossary, acronym = {}, {}
@@ -35,11 +37,11 @@ if __name__ == '__main__':
             for context in context_list:
                 for k, v in glossary.items():
                     f = f.replace(context.format(k), context.format("\\gls{" + v + "}"))
-                    plural = p.plural(v)
-                    f = f.replace(context.format(k), context.format("\\glspl{" + plural + "}"))
+                    plural = p.plural(k)
+                    f = f.replace(context.format(plural), context.format("\\glspl{" + v + "}"))
                     if v[0].capitalize() == v[0]: continue
-                    f = f.replace(context.format(k), context.format("\\Gls{" + v.capitalize() + "}"))
-                    f = f.replace(context.format(k), context.format("\\Glspl{" + plural.capitalize() + "}"))
+                    f = f.replace(context.format(k.capitalize()), context.format("\\Gls{" + v + "}"))
+                    f = f.replace(context.format(plural.capitalize()), context.format("\\Glspl{" + v + "}"))
                 for k, v in acronym.items():
                     f = f.replace(context.format(k), context.format("\\acrshort{" + v + "}"))
             if f != line: print(f)
